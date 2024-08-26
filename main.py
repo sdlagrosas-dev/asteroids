e6a6199b1f5f565ffd1656274c5edc0a7b1d1ddb
sdlagrosas-dev <sdlagrosas.dev@gmail.com>
2024-08-26 12:32:30 +0800

    manage objects using sprite groups instead

--- main.py
+++ main.py
@@ -14,14 +14,25 @@ def main():
 
     player_1 = player.Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    updatable.add(player_1)
+    drawable.add(player_1)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         
         screen.fill(color="black")
-        player_1.update(dt)
-        player_1.draw(screen)
+
+        for obj in updatable:
+            obj.update(dt)
+
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
 
         dt = game_clock.tick(60)/1000
