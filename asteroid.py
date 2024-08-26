2119494743789d9ec0ebb86a43dc2b12578b0596
sdlagrosas-dev <sdlagrosas.dev@gmail.com>
2024-08-26 20:25:24 +0800

    make asteroids lumpy

--- asteroid.py
+++ asteroid.py
@@ -7,12 +7,33 @@ from constants import *
 class Asteroid(circleshape.CircleShape):
     def __init__(self, x, y, radius):
         super().__init__(x, y, radius)
+        self.edges = self.generate_edges(15)
+
+    def generate_edges(self, n):
+        rand_angles = random.sample(range(0, 360, 10), n)
+        rand_angles.sort()
+        rand_points = []
+
+        for angle in rand_angles:
+            vector = pygame.Vector2(0, 1).rotate(angle)
+            point_pos = vector * self.radius
+            rand_points.append(point_pos)
+
+        return rand_points
 
     def draw(self, screen):
         super().draw(screen)
 
-        pygame.draw.circle(
-            screen, color="red", center=self.position, radius=self.radius, width=2
+        # pygame.draw.circle(
+        #     screen, color="red", center=self.position, radius=self.radius, width=2
+        # )
+        edge_update = [self.position + edge for edge in self.edges]
+
+        pygame.draw.polygon(
+            screen,
+            color="red",
+            points=edge_update,
+            width=2
         )
 
     def update(self, dt):
