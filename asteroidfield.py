1222550769477019917bf051ecfe39d2f87f850d
Sam <sdlagrosas.work@gmail.com>
2025-03-19 18:58:23 +0800

    add score system and logic

--- asteroidfield.py
+++ asteroidfield.py
@@ -29,11 +29,11 @@ class AsteroidField(pygame.sprite.Sprite):
     ]
 
     def __init__(self):
-        pygame.sprite.Sprite.__init__(self, self.containers)
+        super().__init__(self.containers)
         self.spawn_timer = 0.0
 
-    def spawn(self, radius, position, velocity):
-        asteroid = Asteroid(position.x, position.y, radius)
+    def spawn(self, radius, position, velocity, kind):
+        asteroid = Asteroid(position.x, position.y, radius, kind)
         asteroid.velocity = velocity
 
     def update(self, dt):
@@ -43,9 +43,9 @@ class AsteroidField(pygame.sprite.Sprite):
 
             # spawn a new asteroid at a random edge
             edge = random.choice(self.edges)
-            speed = random.randint(40, 100)
+            speed = random.randint(ASTEROID_MIN_SPEED, ASTEROID_MAX_SPEED)
             velocity = edge[0] * speed
             velocity = velocity.rotate(random.randint(-30, 30))
             position = edge[1](random.uniform(0, 1))
             kind = random.randint(1, ASTEROID_KINDS)
-            self.spawn(ASTEROID_MIN_RADIUS * kind, position, velocity)
+            self.spawn(ASTEROID_MIN_RADIUS * kind, position, velocity, kind)
---
+++ scoresystem.py
@@ -0,0 +1,35 @@
+import pygame
+
+pygame.font.init()
+
+
+class ScoreSystem(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__(*self.containers)
+        self.destroyed_count = 0
+        self.score = 0
+        self.font = pygame.font.Font(None, 24)
+
+    def draw(self, screen):
+        label = self.font.render(f"Score: {self.score}", True, (255, 255, 255))
+        screen.blit(label, (10, 20))
+
+        destroyed_count_label = self.font.render(
+            f"Destroyed: {self.destroyed_count}",
+            True,
+            (255, 255, 255),
+        )
+        screen.blit(destroyed_count_label, (10, 40))
+
+    def update(self, dt):
+        pass
+
+    def add_score(self, asteroid):
+        kind = asteroid.kind
+        add_score = {
+            1: 10,
+            2: 20,
+            3: 50,
+        }.get(kind, 0)
+        self.score += add_score
+        self.destroyed_count += 1
